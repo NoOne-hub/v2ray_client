15174b0c93c084db276f6f80976888ad932c3c21
NoOne-hub <1270287245@qq.com>
2019-09-19 19:29:06 +0800

    修改manjaro linux的安装脚本

--- install.sh
+++ install.sh
@@ -61,8 +61,6 @@ function install_others() {
     sudo supervisord -c /etc/supervisor/supervisord.conf
     echo "接下来输入update,然后ctrl+d 退出"
     sudo supervisorctl -c /etc/supervisor/supervisord.conf
-    echo -e "success"
-    echo -e "now you can open you web browser,open http://127.0.0.1:8000"
 }
 
 
@@ -111,7 +109,39 @@ function install_prepare_software_on_fedora()
 function install_prepare_software_on_archlinux()
 {
     sudo pacman -S --noconfirm supervisor virtualenv
+    sudo mkdir -m 700 -p /etc/supervisor
+    sudo touch /etc/supervisor/supervisord.conf
+    sudo mkdir -m 700 /etc/supervisor/conf.d
+    sudo echo "; supervisor config file
+
+[unix_http_server]
+file=/var/run/supervisor.sock   ; (the path to the socket file)
+chmod=0700                       ; sockef file mode (default 0700)
+
+[supervisord]
+logfile=/var/log/supervisor/supervisord.log ; (main log file;default \$CWD/supervisord.log)
+pidfile=/var/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
+childlogdir=/var/log/supervisor            ; ('AUTO' child log dir, default \$TEMP)
+
+; the below section must remain in the config file for RPC
+; (supervisorctl/web interface) to work, additional interfaces may be
+; added by defining them in separate rpcinterface: sections
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[supervisorctl]
+serverurl=unix:///var/run/supervisor.sock ; use a unix:// URL  for a unix socket
+
+; The [include] section can just contain the \"files\" setting.  This
+; setting can list multiple files (separated by whitespace or
+; newlines).  It can also contain wildcards.  The filenames are
+; interpreted as relative to this file.  Included files *cannot*
+; include files themselves.
+
+[include]
+files = /etc/supervisor/conf.d/*.conf" > /etc/supervisor/supervisord.conf
 }
+
 
 # 安装opensuse必要软件
 function install_prepare_software_on_opensuse()
@@ -211,6 +241,8 @@ function main()
     second=`expr ${end} - ${begin}`
     min=`expr ${second} / 60`
     echo "It takes "${min}" minutes."
+    echo -e "success"
+    echo -e "now you can open you web browser,open http://127.0.0.1:8000"
 }
 
 main
